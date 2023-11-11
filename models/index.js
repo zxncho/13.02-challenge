1690a16119a40b2e1e3f766ddfd7b275ec91c532
Teej <tyrush1911@outlook.com>
2023-11-11 13:31:28 -0500

    index.js model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag, {
+  through: ProductTag
+  foreignKey:'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  through:ProductTag,
+  foreignKey:'tag_id'
+});
 
 module.exports = {
   Product,
